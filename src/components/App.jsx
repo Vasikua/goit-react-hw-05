c3d8c1a43916adca2fae55bc8733ecf718d47429
Vasyl Mandik <Vasik_ua@ukr.net>
2024-04-28 22:27:29 -0500

    add suspence

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
+import {lazy } from 'react';
 import './App.css';
-import MovieCast from './MovieCast/MovieCast';
-import MovieReviews from './MovieReviews/MovieReviews';
-import HomePage from '../pages/HomePage/HomePage';
 import Layout from './layout/Layout';
-import MoviesPage from '../pages/MoviesPage/MoviesPage';
-import MovieDetailsPage from './MovieDetailsPage/MovieDetailsPage';
-import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
+const HomePage = lazy (() => import ( '../pages/HomePage/HomePage'));
+const MoviesPage = lazy (() => import ( '../pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('../pages/MovieDetailsPage/MovieDetailsPage'));
+const MovieCast = lazy(()=> import( './MovieCast/MovieCast'));
+const MovieReviews = lazy(()=> import( './MovieReviews/MovieReviews'));
+const NotFoundPage = lazy (() => import ( '../pages/NotFoundPage/NotFoundPage'));
+
 
 function App() {
 
   return (
     <Layout>
+      
       <Routes>
         <Route path='/'element={<HomePage/>} />
         <Route path='/movies' element={<MoviesPage />} />
@@ -20,7 +23,8 @@ function App() {
             <Route path='reviews' element={<MovieReviews/>} />
         </Route>
         <Route path='*' element={<NotFoundPage/> } />  
-      </Routes>
+        </Routes>
+        
     </Layout>
   )
 }
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,15 @@
 import css from './Layout.module.css'
 import Navigation from '../navigation/navLink/NavLink';
+import { Suspense } from 'react';
 export default function Layout({ children }) {
     return(
         <div className={css.container}>
             <header className={css.mainNav}>
                 <Navigation />
-                
+                <Suspense fallback={<div>Please wait loading page... </div>}>
+                    {children}
+                </Suspense>
          </header >
-        {children}
+        
     </div>)
 }
